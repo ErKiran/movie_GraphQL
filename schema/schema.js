156da1a07b36639a73d3413116fa53149b88a7bc
Kiran Adhikari <kiruu1238@gmail.com>
2019-08-23 09:26:30 +0545

    Relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,29 +1,53 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 const _ = require('lodash');
 
-var movies = [
-    {
-        id: 1,
-        title: "Death Zone: Cleaning Mount Everest",
-        year: 2018,
-        votes: 65,
-    },
-    {
-        title: "Chhakka Panja 2",
-        id: 2,
-        year: 2017,
-        votes: 261,
-    },
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLList
+} = graphql;
+
+
+const movies = [
+    { name: 'Death Zone: Cleaning Mount Everest', year: 2018, id: '1', director_id: '1' },
+    { name: 'Chhakka Panja 2', year: 2017, id: '2', director_id: '2' },
 ];
+
+const director = [
+    { name: 'Hari Bhakta', age: 45, id: '1' },
+    { name: 'Ram Charan', age: 35, id: '2' }
+]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         year: { type: GraphQLInt },
-        votes: { type: GraphQLInt },
-        title: { type: GraphQLString },
+        name: { type: GraphQLString },
+        director: {
+            type: DirectorType,
+            resolve(parent, args) {
+                return _.find(director, { id: parent.director_id })
+            }
+        }
+    })
+})
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Directors',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parent, args) {
+                return _.filter(movies, { director_id: parent.id })
+            }
+        }
     })
 })
 
@@ -32,9 +56,28 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         movie: {
             type: MovieType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return _.find(movies, { id: args.id });
+            }
+        },
+        director: {
+            type: DirectorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(director, { id: args.id })
+            }
+        },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parent, args) {
+                return movies
+            }
+        },
+        directors: {
+            type: new GraphQLList(DirectorType),
+            resolve(parent, args) {
+                return director
             }
         }
     },
